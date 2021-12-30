71a9535acbb2671291bf447309382c7eedfb6205
gurubaru <71360199+gurubaru@users.noreply.github.com>
2021-12-30 09:16:46 +0800

    Create level1.js

---
+++ aksarabali/level1.js
@@ -0,0 +1,258 @@
+var quiz = {
+  user: "tutlencBlog",
+  questions: [
+	{
+    text: "ring sor puniki sane kawacen BAPA inggih punika",
+    responses: [
+    { text: "ᬩᬯ᭟" },
+    { text: "ᬩᬧ᭟", correct: true },
+    { text: "ᬗᬧ᭟" },
+    { text: "ᬗᬯ᭟" }] },
+
+
+  {
+    text: "Pilih silih sinunggil aksara sane masuara: GA,CA,JA,DA,PA,",
+    responses: [
+    { text: "ᬕ᭞ᬘ᭞ᬚ᭞ᬤ᭞ᬧ᭞", correct: true },
+    { text: "ᬤ᭞ᬳ᭞ᬢ᭞ᬚ᭞ᬲ᭞" },
+    { text: "ᬳ᭞ᬦ᭞ᬘ᭞ᬭ᭞ᬓ᭞" },
+    { text: "ᬫ᭞ᬕ᭞ᬩ᭞ᬗ᭞ᬧ᭞" }] },
+
+
+  {
+    text: "Ritatkala nyurat kruna LALAH pastika wenten aksara tengenan ...",
+    responses: [
+    { text: "surang" },
+    { text: "bisah", correct: true },
+    { text: "adeg-adeg" },
+    { text: "cecek" }] },
+
+
+  {
+    text: "kruna KAJANG patut kasurat sekadi?",
+    responses: [
+    { text: "ᬓᬚᬂ᭟", correct: true },
+    { text: "ᬓᬚᬗ᭄᭟" },
+    {
+      text: "ᬓᬚᬗ᭟" },
+
+    { text: "ᬓᬚᬄ᭟" }] },
+
+
+  {
+    text: "Ring sor puniki, sane kawastanin aksara Anusuara inggih punika",
+    responses: [
+    { text: "pepet, bisah, adeg-adeg, cecek" },
+    {
+      text: "ᬳ᭞ᬦ᭞ᬘ᭞ᬭ᭞ᬓ᭞" },
+
+    { text: "ᬬ᭞ᬭ᭞ᬮ᭞ᬯ᭞" },
+    { text: "ᬜ᭞ᬫ᭞ᬦ᭞ᬗ᭞", correct: true }] },
+
+
+  {
+    text: "kruna DOKAR patut kasurat sekadi",
+    responses: [
+    { text: "ᬤᭀᬓᬭ᭄᭞" },
+    { text: "ᬤᭀᬓᬭ᭞" },
+    { text: "ᬤᭀᬓᬃ᭞", correct: true },
+    { text: "ᬤᭀᬓᬂ᭞" }] },
+
+
+  {
+    text: "Yening nyurat kruna TUNI, pastika pacang wenten Pengangge suara minakadi:",
+    responses: [
+    { text: "suku lan ulu", correct: true },
+    { text: "tedong lan ulu" },
+    {
+      text: "suku lan tedong" },
+
+    { text: "taleng tedong lan taleng" }] },
+
+
+  {
+    text: "Ring sor puniki, sane cen kawastanin aksara sualalita!",
+    responses: [
+    { text: "ᬩ᭞ᬘ᭞ᬦ᭞ᬲ᭞ᬓ" },
+    { text: "ᬡ᭞ᬔ᭞ᬥ᭞ᬣ᭞ᬨ᭞", correct: true },
+    { text: "ᬔ᭞ᬦ᭞ᬩ᭞ᭉ᭞ᬢ᭞" },
+    { text: "ᬳ᭞ᬔ᭞ᬖ᭞ᬣ᭞ᬥ᭞" }] },
+
+
+  {
+    text: "ᬦᬸᬗᬕᬶᬂ᭟ kruna sane masurat antuk aksara bali puniki kawacen!",
+    responses: [
+    {
+      text: "nanggung." },
+
+    { text: "nangging." },
+    { text: "nungaging.", correct: true },
+    { text: "nungging." }] },
+
+
+  {
+    text: "Aksara niki ᬅ᭞ᬇ᭞ᬉ᭞ᬑ᭞᭖᭞ kawastanin ...",
+    responses: [
+    { text: "Pengangge suara" },
+    { text: "Aksara Suara", correct: true },
+    { text: "Modre" },
+    { text: "Aksara wianjana" }] },
+	
+	{
+	text: "\"lele ngamah legu\" surat antuk aksara bali",
+	responses:[
+	{text: "ᬍᬍᬗᬫᬳ᭄ᬮᬾᬕᬸ᭟"},
+	{text: "ᬮᬾᬮᬾᬗᬫᬄᬮᬾᬕᬸ᭟"},
+	{text: "ᬮᬾᬮᬾᬗᬫᬄᬍᬕᬸ᭟", correct: true },
+	{text: "ᬮᬾᬮᬾᬗᬫᬳᬼᬕᬸ᭟"},]},
+{
+	text: "Ring sor sane nenten kabaos \"Pluta\" inggih punika",
+	responses:[
+	{text: "ᬲ᭄ᬫᬺᬢᬶ - ᬲᭂᬫᭂᬃᬢᬶ"},
+	{text: "ᬓ᭄ᬱᬫ - ᬅᬓ᭄ᬱᬫ"},
+	{text: "ᬓ᭄ᬱᬢ᭄ᬭᬶᬬ - ᬲᬢ᭄ᬭᬶᬬ"},
+	{text: "ᬲ᭄ᬭᬶ - ᬳᬲ᭄ᬭᬶ", correct: true },]},
+	{
+	text: "Rngkepan wianjana \"nta, sna, sta\" ring sor sane tan patut inggih punika",
+	responses:[
+	{text: "ᬅᬲ᭄ᬢᬵ᭟"},
+	{text: "ᬅᬱ᭄ᬝ᭟"},
+	{text: "ᬤᬡ᭄ᬝ᭟"},
+	{text: "ᬤᬵᬦ᭄ᬢ᭟", correct: true },]},
+	{
+	text: "Sesuratan rangkepan wianjana \"ssa\" sane tan patut ring sor inggih punika",
+	responses:[
+	{text: "ᬦᬶᬰ᭄ᬰᬾᬲᬵ᭟", correct: true },
+	{text: "ᬤᬸᬰ᭄ᬰᬷᬮ᭟"},
+	{text: "ᬦᬶᬰ᭄ᬰᬾᬱ᭟"},
+	{text: "ᬤᬸᬰ᭄ᬰᬵᬲᬦ᭟"},]},
+	{
+	text: "Ring sor puniki rangkepan wianjana \"sca\" sane tan patut inggih punika",
+	responses:[
+	{text: "ᬦᬶᬰ᭄ᬘᬬ᭟"},
+	{text: "ᬧᬰ᭄ᬘᬵᬢ᭄᭟"},
+	{text: "ᬧᬰ᭄ᬘᬶᬫ᭟"},
+	{text: "ᬦᬷᬲ᭄ᬘᬵᬬ᭟", correct: true },]},
+	{
+	text: "Kruna sane medaging rangkepan \"nca, nja\" aksara \"na\" punika mauwah dados aksara \"nya\". Ring sor puniki sesuratan sane patut inggih punika!",
+	responses:[
+	{text: "ᬤᭀᬜ᭄ᬚᬓ᭟"},
+	{text: "ᬗᬦ᭄ᬘᬦ᭄ᬚᭀᬄ᭟"},
+	{text: "ᬩᬢᬦ᭄ᬢᬶᬫ᭄ᬩᬸᬮ᭄᭟", correct: true },
+	{text: "ᬧᬸᬜᬜ᭄ᬚᬓ᭟"},]},
+	{
+	text: "nyurat \"IPA\" sane patut!",
+	responses:[
+	{text: "᭞ᬳᬶ ᬧᬾ ᬳ᭞"},
+	{text: "᭞ᬇ ᬧᬾ ᬅ᭞", correct: true },
+	{text: "᭞ᬇ ᬧ᭄ ᬅ᭞"},
+	{text: "ᬳᬶᬧ᭟"},]},
+	{
+	text: "Nyurat kruna \"Ketua\" sane patut inggih punika",
+	responses:[
+	{text: "ᬓᬢ᭄ᬯ᭟"},
+	{text: "ᬓᭂᬢ᭄ᬯ᭟"},
+	{text: "ᬓᭂᬢᬸᬯ᭟", correct: true },
+	{text: "ᬓᬢᬸᬯ᭟"},]},
+	{
+	text: "Aksara bali tan kadadosang \"tumpuk tiga\", ring sor puniki, kruna sane tan manut uger-uger inggih punika",
+	responses:[
+	{text: "ᬢᬫ᭄ᬩ᭄ᬮᬶᬗᬦ᭄᭟", correct: true },
+	{text: "ᬇᬦ᭄ᬤ᭄ᬭ᭟"},
+	{text: "ᬓ᭄ᬭ᭄ᬬᬫᬡ᭟"},
+	{text: "ᬢᬦ᭄ᬢ᭄ᬯᬭ"},]},
+	{
+	text: "Aksara Bali sane durung polih pengangge kawastanin aksara napi?",
+	responses:[
+	{text: "Aksara Suara"},
+	{text: "Aksara lagna", correct: true },
+	{text: "Aksara Anuswara"},
+	{text: "aksara Ardasuara"},]},
+	{
+	text: "Aksara bali nganutin sistem suku kata, kawastanin sistem napi?",
+	responses:[
+	{text: "sistem arabik"},
+	{text: "sistem rubrik"},
+	{text: "sistem silabik", correct: true },
+	{text: "sistem alfabet"},]},
+	{
+	text: "Sesuratan kruna \"Panca yadnya\" nganggen aksara Bali sane patut inggih punika",
+	responses:[
+	{text: "ᬧᬜ᭄ᬘᬬᬚ᭄ᬜ᭟", correct: true },
+	{text: "ᬧᬦ᭄ᬘᬬᬤ᭄ᬜ᭟"},
+	{text: "ᬯᬜ᭄ᬘᬬᬚ᭄ᬜ᭟"},
+	{text: "ᬧᬜ᭄ᬘᬬᬤ᭄ᬜ᭟"},]},
+	{
+	text: "Ring sor puniki sane ngranjing ring warga aksara Mahaprana minakadi:",
+	responses:[
+	{text: "ᬅ᭞ᬇ᭞ᬑ᭞"},
+	{text: "ᬔ᭞ᬖ᭞ᬛ᭞ᬡ᭞", correct: true },
+	{text: "ᬅᬁ᭞ᬉᬁ᭞ᬫᬁ᭞"},
+	{text: "ᬫ᭞ᬫᬵ᭞ᬫᬁ᭞"},]},
+	{
+	text: "Warga aksara \"Talawia\" minakadi:",
+	responses:[
+	{text: "ᬓ᭞ᬔ᭞ᬕ᭞ᬖ᭞ᬗ᭞ᬳ᭞"},
+	{text: "ᬢ᭞ᬣ᭞ᬤ᭞ᬥ᭞ᬦ᭞ᬮ᭞ᬲ᭞"},
+	{text: "ᬧ᭞ᬨ᭞ᬩ᭞ᬪ᭞ᬫ᭞ᬯ᭞"},
+	{text: "ᬘ᭞ᬙ᭞ᬚ᭞ᬛ᭞ᬜ᭞ᬬ᭞ᬰ᭞", correct: true },]},
+	{
+	text: "indayang surat kruna \"Rerama\" sane maarti (meme bapa) antuk aksara bali",
+	responses:[
+	{text: "ᬭᬾᬭᬫ᭟"},
+	{text: "ᬭᬭᬫ᭟", correct: true },
+	{text: "ᬋᬭᬫ᭟"},
+	{text: "ᬋᬭᬫᭂ᭟"},]}	] },
+
+
+
+
+userResponseSkelaton = Array(quiz.questions.length).fill(null);
+
+var app = new Vue({
+  el: "#app",
+  data: {
+    quiz: quiz,
+    questionIndex: 0,
+    userResponses: userResponseSkelaton,
+    isActive: false },
+
+  filters: {
+    charIndex: function (i) {
+      return String.fromCharCode(97 + i);
+    } },
+
+  methods: {
+    restart: function () {
+      this.questionIndex = 0;
+      this.userResponses = Array(this.quiz.questions.length).fill(null);
+    },
+    selectOption: function (index) {
+      Vue.set(this.userResponses, this.questionIndex, index);
+      //console.log(this.userResponses);
+    },
+    next: function () {
+      if (this.questionIndex < this.quiz.questions.length)
+      this.questionIndex++;
+    },
+
+    prev: function () {
+      if (this.quiz.questions.length > 0) this.questionIndex--;
+    },
+    // Return "true" count in userResponses
+    score: function () {
+      var score = 0;
+      for (let i = 0; i < this.userResponses.length; i++) {
+        if (
+        typeof this.quiz.questions[i].responses[
+        this.userResponses[i]] !==
+        "undefined" &&
+        this.quiz.questions[i].responses[this.userResponses[i]].correct)
+        {
+          score = score + 1;
+        }
+      }
+      return score;
+
+      //return this.userResponses.filter(function(val) { return val }).length;
+    } } });
