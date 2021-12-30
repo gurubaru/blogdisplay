7722d5fdc9ea661cc2194f599e76199c08fb7070
gurubaru <71360199+gurubaru@users.noreply.github.com>
2021-12-30 16:48:29 +0800

    Create level2.js

---
+++ aksarabali/level2.js
@@ -0,0 +1,230 @@
+var quiz = {
+  user: "tutlencBlog",
+  questions: [
+  {
+    text: "Aksara Ardasuara luir ipun:",
+    responses: [
+    { text: "ᬜ᭞ᬫ᭞ᬦ᭞ᬗ᭞" },
+    { text: "ᬁ"},
+    { text: "ᬬ᭞ᬭ᭞ᬮ᭞ᬯ᭞", correct: true  },
+    { text: "ᬍ᭞ᬋ᭞" }] },
+  {
+    text: "Sesuratan Aksara \"Nga\" ring sor sane tan manut ring uger-uger inggih punika",
+    responses: [
+    { text: "ᬩᬾᬂᬩᬾᬂ᭟" },
+    { text: "ᬘ᭄ᬭᬗ᭄ᬘᬂ᭟", correct: true },
+    { text: "ᬩᬸᬗ᭄ᬲᬶᬮ᭄᭟" },
+    { text: "ᬜᬸᬂᬓ᭄ᬮᬶᬂ᭟" }] },
+  {
+    text: "Tengenan \"Nga\" ring panguntat kruna mauah dados \"cecek\", ring sor puniki sesuratan sane iwang inggih punika",
+    responses: [
+    { text: "ᬩᬮᬂ᭟" },
+    { text: "ᬩᬮᬗ᭄ᬲᬗᬶᬢ᭄᭟", correct: true },
+    { text: "ᬢᬸᬮᬂᬮᬶᬦ᭄ᬤᬸᬂ᭟" },
+    { text: "ᬕᭂᬤᬂ᭟" }] },
+  {
+    text: "Nyurat kruna lingga kalih kecap ring sor sane iwang inggih punika",
+    responses: [
+    { text: "ᬢ᭄ᬯ᭟", correct: true },
+    { text: "ᬲᭂᬮ᭟" },
+    { text: "ᬍᬫᬄ᭟" },
+    { text: "ᬫᭂᬚᬸ᭟" }] },
+  {
+    text: "Ring sor puniki sesuratan aksara ardasuara sakadi suara, sane patut inggih punika",
+    responses: [
+    { text: "ᬢᬩᬶᬬ᭟" },
+    { text: "ᬢᭂᬮᬕ᭟"},
+    { text: "ᬧᬸᬢᭂᬭ᭟" },
+    { text: "ᬲᬢ᭄ᬯ᭟", correct: true  }] },
+  {
+    text: "Sesuratan Aksara \"Ra\" ring sor sane iwang inggih punika",
+    responses: [
+    { text: "ᬢᬮᬶᬦ᭄ᬋᬩᬩ᭄᭟" },
+    { text: "ᬲ᭄ᬫᬺᬢᬶ᭟"},
+    { text: "ᬋᬩᬩ᭄᭟" },
+    { text: "ᬳᬶᬧᬸᬦᬺᬗᬲ᭄᭟", correct: true  }] },
+  {
+    text: "Aksara \"Na\", yening ring ajeng ipun wenten Aksara sane masuara \"R\". Aksara Na mauah dados \"Narambat\". Ring sor sane iwang inggih punika",
+    responses: [
+    { text: "ᬓ᭄ᬭᬸᬡ᭟" },
+    { text: "ᬅᬃᬚᬸᬡ᭟", correct: true },
+    { text: "ᬯᬃᬡ᭟" },
+    { text: "ᬋᬡ᭟" }] },
+  {
+    text: "Ring panyuratan Aksara Bali, wenten Aksara sane tan wenang mapepet, aksara napi manten punika?",
+    responses: [
+    { text: "la lan ra", correct: true  },
+    { text: "gantungan la lan gantungan ra"},
+    { text: "la lan gantungan la" },
+    { text: "ra lan gantungan la" }] },
+  {
+    text: "Sesuratan Aksara \"Ha\" ring sor sane tan manut ring uger-uger inggih punika",
+    responses: [
+    { text: "ᬘᬄᬘᬄ᭟" },
+    { text: "ᬘᬶᬳ᭄ᬦ᭟"},
+    { text: "ᬧᬲᬄ᭟" },
+    { text: "ᬳᬲᬳ᭄ᬤᬸᬭᬾᬦ᭄᭟", correct: true  }] },
+  {
+    text: "Ring sor sesuratan Pangangge tengenan \"Adeg-adeg\" tan kadadosang antuk uger-uger inggih punika",
+    responses: [
+    { text: "ᬢᬫ᭄‌ᬩ᭄ᬮᬶᬗᬦ᭄᭟" },
+    { text: "ᬗᬤᭂᬧ᭄‌ᬧᭀᬄ᭟", correct: true },
+    { text: "ᬲᬶᬭᬗᭂᬫᬶᬢ᭄‌ᬭᬳᬶᬦ᭄ᬳᬶᬤᬦᬾ᭟" },
+    { text: "ᬯᬬᬦ᭄‌ᬲᬸᬢ᭟" }] },
+  {
+    text: "Ring sor punika sane tan nganggen tengenan majalan wantah!",
+    responses: [
+    { text: "ᬳᬮᬸᬦᬮᬸᬦ᭄᭟" },
+    { text: "ᬚᭂᬩᬸᬕᬭᬸᬫ᭄᭟"},
+    { text: "ᬳᬦᬓᬕᬸᬂ᭟" },
+    { text: "ᬳᬮᬗᬦ᭄᭟", correct: true  }] },
+  {
+    text: "Sesuratan Kruna Lingga tigang kecap ring sor sane iwang inggih punika",
+    responses: [
+    { text: "ᬦᬵᬕᬭ᭟" },
+    { text: "ᬫᭂᬚᬮᬦ᭄᭟", correct: true },
+    { text: "ᬓᭂᬢᬶᬧᬢ᭄᭟" },
+    { text: "ᬲᭂᬓᭀᬮᬄ᭟" }] },
+  {
+    text: "Ring uger-uger nyurat Aksara Bali, wenten Dwita sane nenten kamanggehang malih, upami:",
+    responses: [
+    { text: "ᬓᬃᬡ᭄ᬦ᭟", correct: true  },
+    { text: "ᬬᬸᬤ᭄ᬥ᭟"},
+    { text: "ᬘᬶᬢ᭄ᬢ᭟" },
+    { text: "ᬩᬸᬤ᭄ᬥ᭟" }] },
+  {
+    text: "Kruna \"Wangun\" polih pangater \"Nga\" yening surat antuk Aksara Bali dados?",
+    responses: [
+    { text: "ᬗᬯᬗᬸᬦ᭄᭟"},
+    { text: "ᬗ᭄ᬯᬗᬸᬦ᭄᭟", correct: true  },
+    { text: "ᬗᭂᬯᬗᬸᬦ᭄᭟"}] },
+  {
+    text: "Kruna \"Lali\" polih pangater \"Ma\" raris kasurat antuk Aksara Bali dados?",
+    responses: [
+    { text: "ᬫᬮᬮᬶ᭟", correct: true  },
+    { text: "ᬫᬵᬮᬮᬶ᭟"},
+    { text: "ᬫᭂᬮᬮᬶ᭟" },
+    { text: "ᬫ᭄ᬮᬮᬶ᭟" }] },
+  {
+    text: "Kruna \"Tanjung\" polih pangater \Ka\" yening surat antuk Aksara Bali dados?",
+    responses: [
+    { text: "ᬓᬢᬦ᭄ᬚᬸᬂ᭟" },
+    { text: "ᬓ᭄ᬢᬜ᭄ᬚᬸᬂ᭟"},
+    { text: "ᬓᭂᬢᬜ᭄ᬚᬸᬂ᭟" },
+    { text: "ᬓᬢᬜ᭄ᬚᬸᬂ᭟", correct: true  }] },
+  {
+    text: "Kruna Tiron ring sor, sesuratannyane sane tan manut ring Uger-uger inggih punika",
+    responses: [
+    { text: "ᬓ ᬳᬫ᭄ᬩᬶᬮ᭄ - ᬓᬫ᭄ᬩᬶᬮ᭄᭟", correct: true },
+    { text: "ᬲ ᬳᬸᬫᬄ - ᬲᭀᬫᬄ᭟" },
+    { text: "ᬫ ᬳᬸᬫᬄ - ᬫᬸᬫᬄ᭟" },
+    { text: "ᬲ᭄ᬯ ᬅᬃᬣ - ᬲ᭄ᬯᬵᬃᬣ᭟" }] },
+  {
+    text: "Sesuratan lengkara \"Pemerintah ngicenin sesuluh\" sane patut, wantah!",
+    responses: [
+    { text: "ᬧᭂᬫᭂᬭᬶᬦ᭄ᬢᬄᬗᬶᬘᬾᬦᬶᬦ᭄ᬲᭂᬲᬸᬮᬸᬄ᭟" },
+    { text: "ᬧᬫ᭄ᬭᬶᬦ᭄ᬢᬄᬗᬶᬘᬾᬦᬶᬦ᭄ᬲᭂᬲᬸᬮᬸᬄ᭟"},
+    { text: "ᬧᭂᬫ᭄ᬭᬶᬦ᭄ᬢᬄᬗᬶᬘᬾᬦᬶᬦ᭄ᬲᬲᬸᬮᬸᬄ᭟", correct: true  },
+    { text: "ᬧᬫ᭄ᬭᬶᬦ᭄ᬢᬄᬗᬶᬘᬾᬦᬶᬦ᭄ᬲᬲᬸᬮᬸᬄ᭟" }] },
+  {
+    text: "ᬢᬢᬶᬕᬳᬤᬓᭂᬘᬫᬢᬦ᭄᭟ sesuratan latine wantah?",
+    responses: [
+    { text: "tatiga ada kecamatan" },
+    { text: "tetiga ada kecamatan", correct: true },
+    { text: "tatiga ada kacamatan" },
+    { text: "tetiga ada kacamatan" }] },
+  {
+    text: "Aksara anceng \"᭞ᬫ᭞\" rikala nyurat usada mateges?",
+    responses: [
+    { text: "ᬲᭀᬫ᭟" },
+    { text: "ᬫᬸᬗ᭄ᬕᬄ᭟"},
+    { text: "ᬫᬮ᭄ᬮ᭟" },
+    { text: "ᬫᬦ᭄ᬢ᭄ᬭ᭟", correct: true  }] },
+  {
+    text: "Ringkesan pacang kabaos iwang yening kasurat sakadi!",
+    responses: [
+    { text: "᭞᭖ᬲ᭄ ᭖ᬫ᭄ ᬅ᭞" },
+    { text: "᭞ᬧᬾ ᬢᬶᬕ ᬓ᭞"},
+    { text: "ᬩᬮᬶᬢ᭞" },
+    { text: "᭞ᬮᬦ᭄ ᬲ᭄ᬬ᭞", correct: true  }] },
+  {
+    text: "Lengkara \"baas 0,5 kilo\" Aksara baline wantah...",
+    responses: [
+    { text: "ᬩᬵᬲ᭄᭞᭕᭞ᬓᬶᬮᭀ᭟" },
+    { text: "ᬩᬵᬲ᭄ᬢᭂᬗᬄᬓᬶᬮᭀ᭟", correct: true },
+    { text: "ᬩᬵᬲ᭄᭞᭐᭞᭕᭞ᬓᬶᬮᭀ᭟" },
+    { text: "ᬩᬵᬲ᭄᭞᭐᭕᭞ᬓᬶᬮᭀ᭟" }] },
+  {
+    text: "Surat antuk Aksara bali \"Tanggal 30 Desember\"",
+    responses: [
+    { text: "ᬢᬗ᭄ᬕᬮ᭄᭓᭐᭞ᬤᬾᬲᬾᬫ᭄ᬩᭂᬃ᭟" },
+    { text: "ᬢᬂᬕᬮ᭄᭞᭓᭐᭞ᬤᬾᬲᬾᬫ᭄ᬩᭂᬃ᭟"},
+    { text: "ᬢᬗ᭄ᬕᬮ᭄᭞᭓᭐᭞ᬤᬾᬲᬾᬫ᭄ᬩᭂᬃ᭟", correct: true  },
+    { text: "ᬢᬗ᭄ᬕᬮ᭄᭓᭐ᬤᬾᬲᬾᬫ᭄ᬩᭂᬃ᭟" }] },
+  {
+    text: "Ring panelasing nyurat surat, satua, miwah kanda patut nganggen?",
+    responses: [
+    { text: "᭟" },
+    { text: "᭟᭐᭟", correct: true },
+    { text: "᭛᭐᭛" },
+    { text: "᭚" }] },
+  {
+    text: "Sane ten kabaos ceciren pepaosan inggih punika",
+    responses: [
+    { text: "᭟᭐᭟" },
+    { text: "᭛᭐᭛" },
+    { text: "᭚᭐᭚", correct: true },
+    { text: "᭚" }] }  ] },
+
+
+
+
+userResponseSkelaton = Array(quiz.questions.length).fill(null);
+
+var apptutlencBlogQuizL2 = new Vue({
+  el: "#apptutlencBlogQuizL2",
+  data: {
+    quiz: quiz,
+    questionIndex: 0,
+    userResponses: userResponseSkelaton,
+    isActive: false },
+
+  filters: {
+    charIndex: function (i) {
+      return String.fromCharCode(97 + i);
+    } },
+
+  methods: {
+    restart: function () {
+      this.questionIndex = 0;
+      this.userResponses = Array(this.quiz.questions.length).fill(null);
+    },
+    selectOption: function (index) {
+      Vue.set(this.userResponses, this.questionIndex, index);
+      //console.log(this.userResponses);
+    },
+    next: function () {
+      if (this.questionIndex < this.quiz.questions.length)
+      this.questionIndex++;
+    },
+
+    prev: function () {
+      if (this.quiz.questions.length > 0) this.questionIndex--;
+    },
+    // Return "true" count in userResponses
+    score: function () {
+      var score = 0;
+      for (let i = 0; i < this.userResponses.length; i++) {
+        if (
+        typeof this.quiz.questions[i].responses[
+        this.userResponses[i]] !==
+        "undefined" &&
+        this.quiz.questions[i].responses[this.userResponses[i]].correct)
+        {
+          score = score + 1;
+        }
+      }
+      return score;
+
+      //return this.userResponses.filter(function(val) { return val }).length;
+    } } });
